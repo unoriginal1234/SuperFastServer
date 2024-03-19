43286264587b71b4a4be7cac63097269a8ef3ce8
unoriginal1234 <eric.t.roth@gmail.com>
2024-03-19 15:35:19 -0700

    Connected to database

---
+++ db/tableSetup.js
@@ -0,0 +1,37 @@
+const { Client } = require('pg');
+
+const client = new Client({
+  // user: 'username',
+  // password: 'password',
+  host: '127.0.0.1',
+  port: 5432,
+  database: 'mydb',
+});
+
+// Connect to the database
+client.connect()
+  .then(() => {
+    console.log('Connected to PostgreSQL database');
+
+    // Execute SQL queries here
+
+    client.query('SELECT * FROM photos', (err, result) => {
+      if (err) {
+        console.error('Error executing query', err);
+      } else {
+        console.log('Query result:', result.rows);
+      }
+
+      // Close the connection when done
+      client.end()
+        .then(() => {
+          console.log('Connection to PostgreSQL closed');
+        })
+        .catch((err) => {
+          console.error('Error closing connection', err);
+        });
+    });
+  })
+  .catch((err) => {
+    console.error('Error connecting to PostgreSQL database', err);
+  });
--- server/index.js
+++ server/index.js
@@ -1,11 +1,16 @@
 require("dotenv").config()
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
+const sql = require('../db')
+
+app.use(express.json())
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
