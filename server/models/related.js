30edf64cb31b05f26313a5b229bd40868acd79e8
unoriginal1234 <eric.t.roth@gmail.com>
2024-03-27 10:08:16 -0700

    Updated caching middleware to apicache

--- server/models/related.js
+++ server/models/related.js
@@ -1,15 +1,16 @@
 const createClient = require('../../db').createClient;
-const NodeCache = require('node-cache');
-const myCache = new NodeCache({stdTTL: 15000});
+// const NodeCache = require('node-cache');
+// const myCache = new NodeCache({stdTTL: 15000});
 
 module.exports = {
   get: (product_id, callback) => {
-    value = myCache.get(product_id);
+    // value = myCache.get(product_id);
 
-    if (value) {
-      callback(null, value);
-    } else {
+    // if (value) {
+    //   callback(null, value);
+    // } else {
       let client = createClient();
+
       let query = {
         text: 'SELECT related_product_id FROM related WHERE current_product_id = $1',
         values: [product_id]
@@ -20,17 +21,17 @@ module.exports = {
             if (err) {
               callback(err)
             } else {
-              newArr = result.rows.map((rp)=> {
+              callback(null, result.rows.map((rp)=> {
                 return rp.related_product_id
-              })
-              callback(null, newArr)
-              myCache.set(product_id, result.rows[0])
+              }))
+              // myCache.set(product_id, result.rows[0])
               client.end()
             }
           })
         })
+        //.then(() => client.end())
         .catch((error)=> console.log('Error connecting to database'))
-    }
+    //}
 
   }
 }
--- server/routes.js
+++ server/routes.js
@@ -1,19 +1,21 @@
 let router = require('express').Router();
 let controller = require('./controllers');
+const apicache = require('apicache');
+let cache = apicache.middleware;
 
-router.get('/products', controller.products.get)
+router.get('/products', cache('1 minute'), controller.products.get)
 
-router.get('/products/:product_id', controller.products.getById)
+router.get('/products/:product_id', cache('1 minute'), controller.products.getById)
 
-router.get('/cart', controller.cart.get)
+router.get('/cart', cache('1 minute'), controller.cart.get)
 
 router.post('/cart', controller.cart.post)
 
-router.get('/products/:product_id/related', controller.related.get)
+router.get('/products/:product_id/related', cache('1 minute'), controller.related.get)
 
-router.get('/products/:product_id/styles', controller.styles.get)
+router.get('/products/:product_id/styles', cache('1 minute'), controller.styles.get)
 
 //for loader
-router.get('')
+//router.get('')
 
 module.exports = router;
